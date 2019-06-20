41733b3a0454796af4a19167abcb21b339ecd3a4
Blueske <hongshaorouzgy@126.com>
2019-06-20 20:59:31 +0800

    todaytop

---
+++ spider.py
@@ -0,0 +1,94 @@
+import os
+from _md5 import md5
+from multiprocessing.pool import Pool
+from urllib.parse import urlencode
+
+import requests
+import time
+
+'''
+aid=24
+app_name=web_search
+offset=20
+format=json
+keyword=%E8%A1%97%E6%8B%8D
+autoload=true
+count=20
+en_qc=1
+cur_tab=1
+from=search_tab
+pd=synthesis
+timestamp=1561031332465'''
+
+def get_page(offset):
+    params = {
+        'aid': '24',
+        'app_name': 'web_search',
+        'offset': offset,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'en_qc': '1',
+        'cur_tab': '1',
+        'from': 'search_tab',
+        'pd': 'synthesis',
+        'timestamp': time.time()
+    }
+
+    headers = {
+        'accept': 'application/json, text/javascript',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'user - agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    url = 'https://www.toutiao.com/api/search/content/?' + urlencode(params)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError:
+        return None
+
+
+def get_images(json):
+    if json.get('data'):
+        for item in json.get('data'):
+            title = item.get('title')
+            images = item.get('image_detail')
+            for image in images:
+                yield {
+                    'image': image.get('url'),
+                    'title': title
+                }
+
+
+def save_image(item):
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+
+    try:
+        response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print('Alreadt Doenloaden', file_path)
+
+    except requests.ConnectionError:
+        print('Failed to Save Image')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+if __name__ == '__main__':
+    main(0)
